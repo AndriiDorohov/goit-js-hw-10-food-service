54151fc00c37163500b35a49562635c97a648388
Andrii Dorohov <wedpositive@gmail.com>
2021-03-20 14:36:49 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import './styles.css';
+import './js/themes.js';
+import './js/cards.js';
+
---
+++ src/js/cards.js
@@ -0,0 +1,7 @@
+import cardsTemplates from '../templates/cards.hbs';
+import menu from '../menu.json'
+
+const markup = cardsTemplates(menu);
+
+const menuRef = document.querySelector('.js-menu');
+menuRef.insertAdjacentHTML('beforeend', markup);
